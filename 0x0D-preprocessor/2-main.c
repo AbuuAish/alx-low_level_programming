81dae4e615f1988a07aa2fd308d392ddb9ef4177
AbuuAish <ayuba.shakiru@gmail.com>
2023-01-16 21:57:13 -0800

    Task2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ *  *main - prints the name of the file
+ *   *
+ *    *Return: always 0
+ *    */
 
-int main()
+int main(void)
+	
 {
 	printf("File name is: %s\n", __FILE__);
 	return 0;
